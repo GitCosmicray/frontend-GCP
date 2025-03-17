7f791e5455aa8b182302f9c41fa9ee89ee4ae93c
Ezhil B <bootlabs@Ezhils-MacBook-Pro.local>
2025-03-17 11:20:35 +0530

    jenkins groovy code commit

---
+++ JenkinsFile.groovy
@@ -0,0 +1,81 @@
+pipeline {
+    agent any
+
+    environment {
+        PROJECT_ID = 'consummate-rig-453502-q2'   
+        REGION = 'us-central1'                   
+        REPO_NAME = 'private-image-repo'         
+        IMAGE_NAME = 'test-react1'               
+        SONAR_HOST_URL = 'http://localhost:9000' 
+        SCANNER_HOME=tool 'sonar-scanner'
+    }
+
+    stages {
+
+        stage('Clone Repository') {
+            steps {
+                git branch: 'master', url: 'https://github.com/GitCosmicray/frontend-GCP.git'
+            }
+        }
+
+        stage('Authenticate with GCP') {
+            steps {
+                withCredentials([file(credentialsId: 'GCP-jenkins-json', variable: 'GOOGLE_CREDENTIALS_FILE')]) {
+                    sh '''
+                    export GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_CREDENTIALS_FILE
+                    gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+                    gcloud auth configure-docker $REGION-docker.pkg.dev
+                    '''
+                }
+            }
+        }
+
+        stage('Run SonarQube Analysis') {
+            steps {
+                withSonarQubeEnv('SonarQube') {
+                    sh '''
+                    sonar-scanner \
+                    -Dsonar.projectKey=my-app \
+                    -Dsonar.sources=. \
+                    -Dsonar.host.url=$SONAR_HOST_URL
+                    '''
+                }
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    sh '''
+                    docker build -t $REGION-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/$IMAGE_NAME:latest .
+                    '''
+                }
+            }
+        }
+
+        stage('Push to Artifact Registry') {
+            steps {
+                script {
+                    sh '''
+                    docker push $REGION-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/$IMAGE_NAME:latest
+                    '''
+                }
+            }
+        }
+
+        stage('Deploy to Cloud Run') {
+            steps {
+                script {
+                    sh '''
+                    gcloud run deploy $IMAGE_NAME \
+                        --image $REGION-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/$IMAGE_NAME:latest \
+                        --platform managed \
+                        --region $REGION \
+                        --allow-unauthenticated
+                        --port=80
+                    '''
+                }
+            }
+        }
+    }
+}
